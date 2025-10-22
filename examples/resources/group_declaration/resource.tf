daf0257292439159fe362106cd3a2ba3ff3501bd
akline-palantir <akline@palantir.com>
2025-10-22 12:00:59 -0400

    Update examples/resources/group_declaration/resource.tf
    Co-authored-by: eashanpanjwani <209253037+eashanpanjwani@users.noreply.github.com>

--- examples/resources/group_declaration/resource.tf
+++ examples/resources/group_declaration/resource.tf
@@ -1,4 +1,4 @@
-resource "foundry_group_declaration" "example-group" {
+resource "foundry_group" "example-group" {
   name        = "Example group name"
   description = "An example group in Foundry"
   organizations = ["example-organization-rid"]
